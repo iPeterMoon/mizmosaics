eb4ad39b7a8f0d2b5c0757ba2f15ba85aa76165e
Pedro Luna <106363641+iPeterMoon@users.noreply.github.com>
2026-02-23 11:09:19 -0700

    Agregué un placehold

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,14 @@ export default function Home() {
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full flex-col  bg-white ">
         <Navbar/>
+        <div className="w-full relative">
+          <Image
+            src="https://placehold.co/1800x600/png"
+            alt="Hero Image"
+            width={1800}
+            height={600}
+          />
+        </div>
       </main>
     </div>
   );
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
 };
+
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+        search: ''
+      },
+    ],
+  },
+}
+
 
 export default nextConfig;
