440977d985d7fac879bac606d050a7dad1ce6a58
Pedro Luna <106363641+iPeterMoon@users.noreply.github.com>
2026-03-10 12:17:30 -0700

    Next Config build

--- src/app/api/test-db/route.ts
+++ src/app/api/test-db/route.ts
@@ -1,8 +1,6 @@
 import { NextResponse } from "next/server";
 import { prisma } from '@/src/lib/prisma';
 
-export const dynamic = 'force-dynamic';
-
 export async function GET() {
     try {
         const nuevoUsuario = await prisma.user.create({
